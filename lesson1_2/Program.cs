fc3a33fbfcdf52ce08dfb5220ee8f1fb4457e284
Svetlana Svetlanova <dru19051992@gmail.com>
2023-06-06 23:24:02 +0300

    lesson1_array

---
+++ lesson1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] ar = {0, 1, 2, 3, 5, 6, 7, 8};
+for (int i=0; i<8; i++) 
+   {
+     Console.Write($"{ar[i]} ");
+   }
+
+Console.WriteLine();
+
+foreach (int elem in ar)
+{
+    Console.Write($"{elem} ");
+}
